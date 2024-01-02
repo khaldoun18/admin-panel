5ac8f9ea6e3b32e252a454f82632e5a31e7405df
khaldoun <khaldounkhishin7@gmail.com>
2024-01-02 15:02:15 +0300

    added dependent select

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -17,7 +17,7 @@ class StateResource extends Resource
 {
     protected static ?string $model = State::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-flag';
+    protected static ?string $navigationIcon = 'heroicon-o-building-library';
 
     protected static ?string $navigationGroup='System Management';
 
@@ -27,9 +27,11 @@ class StateResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('country_id')
-                    ->required()
-                    ->numeric(),
+                Forms\Components\Select::make('country_id')
+                ->relationship(name:'country' ,titleAttribute:'name')
+                ->searchable()
+                ->preload()
+                ->required(),
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('logo_black.png') }}" alt="logo" class="h-10">
