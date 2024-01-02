99a2e692b4a222b27002e4887813ae4efe704e0e
khaldoun <khaldounkhishin7@gmail.com>
2024-01-02 15:10:47 +0300

    inserted first employee

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -61,8 +61,12 @@ class EmployeeResource extends Resource
                 Forms\Components\Section::make('User Dates')
                     ->schema([
                         Forms\Components\DatePicker::make('date_of_birth')
+                        ->native(false)
+                        ->displayFormat('d/m/Y')
                             ->required(),
                         Forms\Components\DatePicker::make('date_hired')
+                        ->native(false)
+                        ->displayFormat('d/m/Y')
                             ->required(),
                     ])->columns(2),
 
